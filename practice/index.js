d221b6af3718a5f1512b435051f4feb8d1998161
swapyshelke <swapyshelke02gmail.com>
2024-03-31 21:16:56 +0530

    learned new things

--- practice/index.js
+++ practice/index.js
@@ -1,16 +1,15 @@
 // const user = {
-    //     age: 20
-    // }
-    
-    // user.age = 40
-    // console.log(user.age);
+//     age: 20
+// }
 
-    // const arrayOfObjects = [
+// user.age = 40
+// console.log(user.age);
+
+// const arrayOfObjects = [
 //     { name: 'Alice', age: 25 },
 //     { name: 'Bob', age: 30 },
 //     { name: 'Charlie', age: 35 }
 // ];
-
 
 // for(let i = 0 ; i < arrayOfObjects.length ; i++) {
 // console.log(arrayOfObjects[i]);
@@ -32,13 +31,11 @@
 
 // console.log(names);
 
-
-
 // 1. register
 // 2. send welcome email
-// 3. login 
-// 4. get user data 
-// 5. display user data 
+// 3. login
+// 4. get user data
+// 5. display user data
 
 // function register(){
 //     setTimeout(() => {
@@ -49,7 +46,7 @@
 // function sendEmail(){
 //     setTimeout(() => {
 //         console.log("Send welcome function");
-       
+
 //     }, 2000)
 // }
 
@@ -71,13 +68,11 @@
 //     }, 2000);
 // }
 
-
 // register()
 // sendEmail()
 // login()
 // getUserData()
 // displayUserData()
-
 
 // register(() => {
 //     sendEmail(() => {
@@ -271,36 +266,28 @@
 
 // =================
 
-
-
 // create function which takes string as input and output it as uppercase
-
 
 // return uppercase vale from same function
 
+//
 
-// 
-
-// 1. functions loggin value 
+// 1. functions loggin value
 // 2. functions taking parameters
 // 3. function returning values
 // 4. default parameter value
-// 5. variable scope 
+// 5. variable scope
 // 6. function declaration
 // 7. function expression
 // 8. callback functions
-// 9. IIFE'S   
+// 9. IIFE'S
 // 10. Arrow functon
-
-
-
-
 
 // 1. register
 // 2. send welcome email
-// 3. login 
-// 4. get user data 
-// 5. display user data 
+// 3. login
+// 4. get user data
+// 5. display user data
 
 // function register(){
 //     return new Promise((res, rej) => {
@@ -347,8 +334,7 @@
 //    })
 // }
 
-
-// single threaded 
+// single threaded
 
 // register(() => {
 //     sendEmail(() => {
@@ -360,11 +346,9 @@
 //     })
 // })
 
-
 // register().then(sendEmail).then(login).then(getUserData).then(displayUserData).catch((error) => {
 //     console.log("error: " + error);
-// }) 
-
+// })
 
 // async function authemticate(){
 //     await register()
@@ -387,14 +371,12 @@
 // login("Sahil", "sahilspass")
 // console.log('aashay', 'aashayspass');
 
-
 // function uppercase(string){
 //     const str = string.toUpperCase();
 //     return str;
 // }
 
 // console.log();
-
 
 // named export
 
@@ -406,3 +388,131 @@
 // import logout from "./one.js";
 
 // logout()
+
+// 9.85986u56289
+
+// 9.85 o r9.8598
+
+// let a = 9.9865298762
+
+// let b = a.toFixed(2)
+
+// console.log(typeof b);
+
+// let number = 9.9865298762;
+// // let fixedNumber = Number(number.toFixed(2)); // Outputs 9.98
+// // console.log(fixedNumber);
+
+// number = Math.trunc(number*Math.pow(10, 2))/Math.pow(10, 2)
+// console.log(number);
+
+// let text = "Hello World!";
+// let result = text.toLocaleLowerCase();
+// console.log(result);
+
+// let str = "Hello World!"; // "Hello World" in Hindi
+// let lowerCaseStr = str.toLowerCase(); // Outputs "नमस्ते दुनिया" based on Unicode rules
+// let localeLowerCaseStr = str.toLocaleLowerCase('hi-IN'); // Outputs "नमस्ते दुनिया" based on Hindi locale
+
+// console.log(lowerCaseStr); // "नमस्ते दुनिया"
+// console.log(localeLowerCaseStr);
+
+// const name = "Swapnil Shelke"
+
+// const fname = name.substr(0, 7);
+// console.log(fname);
+
+// const url = "https://www.example.com/path/to/page"
+
+// const str = url.substring(8).split("/")[0];
+
+// console.log(str);
+
+// let longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+// const short = longText.length > 20 ? longText.substring(0, 20) + "..." : longText;
+// console.log(short);
+
+// let string = 'I love JavaScript. If you do not love JavaScript what else can you love.'
+// console.log(string.match('love'))
+
+// let string = 'love'
+// let patternOne =' /love/ '    // with out any flag
+// let patternTwo = '/love/gi'   // g-means to search in the whole text, i - case insensitive
+
+// console.log(patternOne);
+// console.log(patternTwo);
+
+// let string = 'I love JavaScript. I love programming.';
+// // let newString = string.replace(/love/g, 'like');
+// // console.log(newString);
+
+// console.log(string.repeat(10));
+
+// console.log(typeof NaN);
+
+// let number = "100"
+
+// let newnum = +number
+// console.log( newnum);
+
+// console.log(parseInt('abed123', 16));
+
+// const str = "30 Days Of JavaScript";
+// // const ans = str.match(/a/gi)
+// // console.log(ans);
+
+// const newarr = []
+
+// str.forEach((element, index) => {
+//   if (element.toLocaleLowerCase() == "a") {
+//     newarr.push(index)
+//   }
+// });
+
+// console.log(newarr);
+
+// // for(let i = 0 ; i < str.length ; i++) {
+// //     if(str[i].toLocaleLowerCase() == 'a')
+// //     newarr.push(i)
+// // }
+
+// console.log(newarr);
+
+
+// const nameone = 'Python';
+// const nametwo = 'Jargon';
+
+// console.log(nameone.includes('on'));
+// console.log(nametwo.includes('on'));
+
+// for(let i = 0 ; i < 100 ; i++) {
+// let randomnum = Math.floor(Math.random() * 256)
+// // randomnum = randomnum >= 255 ? randomnum : randomnum + 50; 
+// console.log(randomnum);
+// }
+
+
+// const name = 'JavaScript'
+
+// // console.log(num);
+
+// for(let i = 0 ; i < 10 ; i++) {
+//     const num  = Math.round(Math.random()  * name.length ) 
+//     console.log(name[num]);
+// }
+
+
+// console.log("1 1 1 1 1 \n 2 1 2 4 8 3 \n 1 3 9 27 4 1 \n 4 16 64 5 1 \n 5 25 125");
+
+const sentence = 'You cannot end a sentence with because because because is a conjunction';
+const phrase = 'because because because';
+
+const indexone = sentence.indexOf(phrase)
+console.log(indexone);
+const indextwo = indexone + phrase.length
+console.log(indextwo);
+
+const ans = sentence.substring(0, indexone) + sentence.substring(indextwo) 
+
+console.log(ans);
