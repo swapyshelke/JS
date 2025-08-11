38e78dfdc50899802313375abdf445f8f8a20c48
swapyshelke <swapyshelke02gmail.com>
2025-08-11 23:50:10 +0530

    Time and Date

---
+++ practice/Basics/index.js
@@ -0,0 +1,12 @@
+const now = new Date();
+// console.log(now.toString());
+
+// const formattedDate = `${now.getDate()} / ${now.getMonth() + 1} / ${now.getFullYear()}`;
+
+// console.log(formattedDate);
+
+const formattedTime = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`
+
+console.log(formattedTime);
+
+
