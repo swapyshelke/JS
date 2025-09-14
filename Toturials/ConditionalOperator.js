789fc1790c8e5b2030aa82ebffc2720134d70978
swapyshelke <swapyshelke02gmail.com>
2025-09-14 23:59:15 +0530

    hof and conditional operator

---
+++ Toturials/ConditionalOperator.js
@@ -0,0 +1,10 @@
+// iss video me ham conditional ya ternary operator sikhane wale he
+
+// ye bilkul if else ke jaise hi kaam karta he
+
+// isme aaur if else me chota sa hi difference hota he
+
+// ternary operator me ek conditon hoti he
+// har condition ke do hi output ho sakte he
+// ya to wo true hoga ya to wo false hoga
+
