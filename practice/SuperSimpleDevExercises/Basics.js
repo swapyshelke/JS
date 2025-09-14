01d261fd80b767f056484b1c8931ff37b0a4fe45
swapyshelke <swapyshelke02gmail.com>
2025-09-15 00:38:31 +0530

    eercise

---
+++ practice/SuperSimpleDevExercises/Basics.js
@@ -0,0 +1,20 @@
+alert("goog morning")
+
+alert("swapnil")
+
+10 + 5
+
+20 - 5
+
+2 + 2 - 5
+
+document.body.innerHTML = 'good morning'
+
+document.body.innerHTML = 'swapnil shelke'
+
+10 + 8 + 20
+
+100 - 20 - 50 + 200
+
+document.body.innerHTML = ""
+// to make web page blank
