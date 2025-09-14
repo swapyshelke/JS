ecf8e988a057b8ae6e7bd71c91360aec1490b716
swapyshelke <swapyshelke02gmail.com>
2025-09-15 00:34:10 +0530

    basics

---
+++ SuperSimpleDEV/_01_basics.js
@@ -0,0 +1,31 @@
+// JS Basics
+
+// JS adds logic, interaction to websites
+// what to do after clicking on a button
+
+// supersimple.dev/js-basics
+
+
+// alert("success");
+
+// terminology / naming
+// - instructions given to computer : code
+// programming languages : JS, Java
+
+// alert() - JS language
+
+// Math 
+// - 2 + 2
+
+// - 10 / 2
+
+// syntax: rules that we have to follow 
+// when using a programming language
+
+// similar to english
+
+//// in this lesson we learned that 
+// 1. JS is giving instruction to a computer
+// 2. Wrote JS code
+// 3. Run our code using the console
+// 4. syntax
