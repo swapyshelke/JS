7dc3b6258fbcd0cc4cfcd25a860071f3574616a0
swapyshelke <swapyshelke02gmail.com>
2025-11-19 02:45:43 +0530

    becoming JS ninja

---
+++ practice/All-Methods/String/2.js
@@ -0,0 +1,9 @@
+let str = "hello world"
+
+
+// accessing or pointing to last letter of string
+console.log(str[str.length - 1]);
+
+
+// pointing to first letter or char of string
+console.log(str[0]);
