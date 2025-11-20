6aada50dea6e3a7b0cee91d9e09a4be2a86e3f45
swapyshelke <swapyshelke0@gmail.com>
2025-11-20 23:45:54 +0530

    Update 1.js

--- interview/dry-run/IntermediateLEvel/1.js
+++ interview/dry-run/IntermediateLEvel/1.js
@@ -1,4 +1,14 @@
 // Flatten a nested array (one level only)
 
-
 const arr = [1, [2, 3], 4, [5, 6]];
+
+const flattened = arr.reduce((acc, item) => {
+  if (Array.isArray(item)) {
+    return acc.concat(item);
+  }
+  return acc.concat([item]);
+}, []);
+
+console.log(flattened); 
+
+
